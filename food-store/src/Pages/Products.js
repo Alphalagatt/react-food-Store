4594668dee2d046245d9557144aacc71564a08b5
Alpha Lagatt <lagattalpha@gmail.com>
2021-11-25 10:15:58 +0300

    Added components for use with redux

--- food-store/src/Pages/Products.js
+++ food-store/src/Pages/Products.js
@@ -1,6 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, multiply } from "../Components/ComponentCss/ReduxTools/Actions";
+
 function Products(){
+    const counter = useSelector(state => state.counter); 
+    const dispatch = useDispatch();
+    function add(){
+        dispatch(increment());
+    }
+    function remove(){
+        dispatch(decrement());
+    }
+    function mult(){
+        dispatch(multiply());
+    }
     return(
-        <div>This is my products page..</div>
+        <div>
+            <p>This is my products page..</p>
+            <button onClick={add}>+</button>
+            <p>Counter {counter} <button onClick={mult}>x2</button> </p>
+            <button onClick={remove}>-</button>
+            
+        </div>
     )
 }
 
